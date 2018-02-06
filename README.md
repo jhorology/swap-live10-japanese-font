6176aaae75453ea1bfeea743f7729c5ba7eb8179
jhorology <jhorology2014@gmail.com>
2018-02-07 00:37:05 +0900

    add support for Push2 font

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Live 10用日本語フォントを任意のフォントに置き換えるスク�
 Live 10のフォントの日本語部分はNoto Sans CJK JPフォントが使用されていますが、以下の点を改善すべく色々なフォントを試す事を目的としたスクリプトです。
  - 私個人の環境(retina 27inch 5k Liveでのスケール120%)では読み辛い、細すぎ。
  - ブラウザにおいて英字とのバランスが悪く、間延びした感。
+ - なぜかPush2用の日本語フォントはインストールされておらず、おそらくQtのfallbackでOSのフォントが使用されておりWindowsとmacOSで違うフォントが使われるという...
 
-幸い日本語/英字で別々のフォントファイルが使用されているため、デバイス、セッション、アレンジビュー等その他のUIに影響を与える事なく日本語部分だけを変更する事が可能なようです。
-また日本語フォントファイルはファイル名のみで参照されており容易に置き換え可能でした。
+幸いLive10では日本語/英字で別々のフォントファイルが使用されているため、デバイス、セッション、アレンジビュー等その他のUIに影響を与える事なく日本語部分だけを変更する事が可能なようです。
+また日本語フォントファイルはファイル名のみで参照されており容易に置き換え可能でした。Push2用のフォントは日本語フォントをオリジナルフォントにマージしています。
 
 ### 実行環境
 
@@ -26,26 +27,32 @@ echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >>
 ### フォントのインストール
 Liveが閉じている事を確認し、ターミナルでスクリプトを実行してください。
 ```
-swap-live10-japanese-fonts.sh [-e edition] regular_font bold_font
+swap-live10-japanese-fonts.sh [-e edition] regular_font bold_font [push2_browser_font]
 ```
  - -e edition
  Liveアプリケーション名の接尾語(beta|intro|lite|standard|suite)を指定します。省略時デフォルトは"suite"です。
  - regular_font
- Regularフォントのパスを指定します。指定可能なファイルの拡張子は.ttfまたは.otfです。
- - bold_font
- Boldフォントのパスを指定します。指定可能なファイルの拡張子は.ttfまたは.otfです。
- - ttfファイルを指定した場合、otfファイルへ変換を行います。
- - 変更前の元ファイルは.origの拡張子を付け退避します。既に退避したファイルがある場合は上書きしません。
+ Live10で使用する日本語Regularフォントのパスを指定します。指定可能なファイルの拡張子は.ttfまたは.otfです。
+指定したフォントをNotoSansCJKjp-Regular.otfに置き換えます。
+- bold_font
+ Live10で使用する日本語Boldフォントのパスを指定します。指定可能なファイルの拡張子は.ttfまたは.otfです。
+指定したフォントをNotoSansCJKjp-Bold.otfに置き換えます。
+ - push2_browser_font オプショナル
+Push2のブラウザで使用する日本語フォントのパスを指定します。指定可能なファイルの拡張子は.ttfまたは.otfです。
+指定したフォントをオリジナルのAbletonSansLight-Regular.otfとマージし置き換えます。
+- ttfファイルを指定した場合、otfファイルへ変換を行います。
+- 変更前の元フォントファイルは.origの拡張子を付け退避します。既に退避したファイルがある場合は上書きしません。
 
 実行例:
 ```
-swap-live10-japanese-fonts.sh -e beta mgenplus-2cp-medium.ttf mgenplus-2cp-heavy.ttf
+swap-live10-japanese-fonts.sh -e suite mgenplus-2cp-medium.ttf mgenplus-2cp-heavy.ttf　mgenplus-2cp-regular.ttf
 ```
 
-以下の2ファイルが置き換えられます。
+以下の2ファイルとオプションでpush2用の1ファイルが置き換えられます。
 ```
-/Applications/Ableton Live 10 Beta.app/Contents/App-Resources/Fonts/NotoSansCJKjp-Regular.otf
-/Applications/Ableton Live 10 Beta.app/Contents/App-Resources/Fonts/NotoSansCJKjp-Bold.otf
+/Applications/Ableton Live 10 ${LIVE10_EDITION}.app/Contents/App-Resources/Fonts/NotoSansCJKjp-Regular.otf
+/Applications/Ableton Live 10 ${LIVE10_EDITION}.app/Contents/App-Resources/Fonts/NotoSansCJKjp-Bold.otf
+/Applications/Ableton Live 10 ${LIVE10_EDITION}.app/Contents/Push2/Push2DisplayProcess.app/Contents/Push2/qml/Ableton/Appearance/fonts/AbletonSansLight-Regular.otf
 ```
 
 ### アンインストール
@@ -65,5 +72,6 @@ swap-live10-japanese-fonts.sh [-e edition] -u
 <img src="https://raw.githubusercontent.com/jhorology/swap-live10-japanese-font/img/img/genei.png" width="525"/>
 
 ### Notes
+ - Push2用フォントのマージにおいて指定するフォントによってはうまくいかずfontoforgeが落ちます(GenEiGothicP等)。
  - 本スクリプトおよび生成されたフォントの使用に関連する障害その他一切の責任は使用者が負うものとします。
  - 本スクリプトの改定、配布は自由に行ってください。筆者への連絡不要です。
--- swap-live10-japanese-fonts.sh
+++ swap-live10-japanese-fonts.sh
@@ -7,7 +7,7 @@
 usage_exit() {
     echo
     echo "Usage:"
-    echo "  To install: $0 [-e edition] regular_font bold_font" 1>&2
+    echo "  To install: $0 [-e edition] regular_font bold_font [push2_browser_font]" 1>&2
     echo "    edition: beta|lite|intro|standard|suite   default=suite" 1>&2
     echo
     echo "  To uninstall: $0 -u" 1>&2
@@ -36,23 +36,42 @@ validate_src_font() {
     fi
 }
 
-#  $1: src ttf font filet
-#  $2: dest otf font filet
+#  $1: src ttf font file
+#  $2: dest otf font file
 ttf2otf() {
     fontforge -c '
 import fontforge
-font = fontforge.open("'$1'")
-font.generate("'$2'")
+font = fontforge.open("'"${1}"'")
+font.generate("'"${2}"'")
+'
+}
+
+#  $1: original font path
+#  $2: japanese font path
+#  $3: save path
+merge_font () {
+    fontforge -c '
+import fontforge
+font = fontforge.open("'"${1}"'")
+font.mergeFonts("'"${2}"'")
+font.generate("'"${3}"'")
 '
 }
 
 if [ ! "`uname`" = "Darwin" ]; then
     echo
-    echo "Unsupported architecture."
+    echo "Unsupported architecture. this script support only macOS."
     echo
     exit 1
 fi
 
+num=$(ps aux | grep 'Ableton Live 10' | wc -l)
+if [ $num -gt 1 ]; then
+    echo
+    echo "Ableton Live is running, please close it."
+    echo
+    exit 1
+fi
 # options
 LIVE10_EDITION='Suite'
 UNINSTALL=false
@@ -70,58 +89,100 @@ do
     esac
 done
 shift $((OPTIND - 1))
-SRC_REGULAR_FONT=$1
-SRC_BOLD_FONT=$2
+JP_REGULAR_FONT=$1
+JP_BOLD_FONT=$2
+JP_PUSH2_FONT=$3
 
-REGULAR_OTF=NotoSansCJKjp-Regular.otf
-BOLD_OTF=NotoSansCJKjp-Bold.otf
-LIVE10_FONTS="/Applications/Ableton Live 10 ${LIVE10_EDITION}.app/Contents/App-Resources/Fonts"
+LIVE10_FONTS_DIR="/Applications/Ableton Live 10 ${LIVE10_EDITION}.app/Contents/App-Resources/Fonts"
+PUSH2_FONTS_DIR="/Applications/Ableton Live 10 ${LIVE10_EDITION}.app/Contents/Push2/Push2DisplayProcess.app/Contents/Push2/qml/Ableton/Appearance/fonts"
+LIVE10_REGULAR_OTF=NotoSansCJKjp-Regular.otf
+LIVE10_BOLD_OTF=NotoSansCJKjp-Bold.otf
+PUSH2_BROWSER_OTF=AbletonSansLight-Regular.otf
 
 # uninstall
 if $UNINSTALL ; then
-    if [ -f "${LIVE10_FONTS}/${REGULAR_OTF}.orig" ]; then
-        mv -f "${LIVE10_FONTS}/${REGULAR_OTF}.orig" "${LIVE10_FONTS}/${REGULAR_OTF}"
+    if [ -f "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}.orig" ]; then
+        mv -f "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}.orig" "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}"
     else
-        echo -e "\nfile: ${LIVE10_FONTS}/${REGULAR_OTF}.orig does not exist.\n"
-        exit 1
+        echo -e "\nfile: ${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}.orig does not exist.\n"
     fi
-    if [ -f "${LIVE10_FONTS}/${BOLD_OTF}.orig" ]; then
-        mv -f "${LIVE10_FONTS}/${BOLD_OTF}.orig" "${LIVE10_FONTS}/${BOLD_OTF}"
+    if [ -f "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}.orig" ]; then
+        mv -f "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}.orig" "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}"
     else
-        echo -e "\nfile: ${LIVE10_FONTS}/${BOLD_OTF}.orig does not exist.\n"
-        exit 1
+        echo -e "\nfile: ${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}.orig does not exist.\n"
+    fi
+    if [ -f "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}.orig" ]; then
+        mv -f "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}.orig" "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}"
     fi
     exit 0
 fi
 
-if [ -z $SRC_REGULAR_FONT ]; then
+if [ -z "${JP_REGULAR_FONT}" ]; then
     echo -e "\nregular_font is not specified."
     usage_exit
 fi
 
-if [ -z $SRC_BOLD_FONT ]; then
+if [ -z "${JP_BOLD_FONT}" ]; then
     echo -e "\nbold_font is not specified."
     usage_exit
 fi
 
-if (validate_src_font $SRC_REGULAR_FONT); then
-    # TTF Fonr
-    ttf2otf $SRC_REGULAR_FONT $REGULAR_OTF
-    SRC_REGULAR_FONT=$REGULAR_OTF
+if (validate_src_font "${JP_REGULAR_FONT}"); then
+    # TTF Font
+    ttf2otf "${JP_REGULAR_FONT}" /tmp/_temporary_live10_jp_regular_font.otf
+    JP_REGULAR_FONT=/tmp/_temporary_live10_jp_regular_font.otf
 fi
-if (validate_src_font $SRC_BOLD_FONT); then
-    # TTF Fonr
-    ttf2otf $SRC_BOLD_FONT $BOLD_OTF
-    SRC_BOLD_FONT=$BOLD_OTF
+if (validate_src_font "${JP_BOLD_FONT}"); then
+    # TTF Font
+    ttf2otf "${JP_BOLD_FONT}" /tmp/_temporary_live10_jp_bold_font.otf
+    JP_BOLD_FONT=/tmp/_temporary_live10_jp_bold_font.otf
+fi
+if [ ! -z "${JP_PUSH2_FONT}" ] && (validate_src_font "${JP_BOLD_FONT}"); then
+    # TTF Font
+    ttf2otf "${JP_PUSH2_FONT}" /tmp/_temporary_push2_jp_browser_font.otf
+    JP_PUSH2_FONT=/tmp/_temporary_push2_jp_browser_font.otf
 fi
 
-# swap fonts
-if [ ! -f "${LIVE10_FONTS}/${REGULAR_OTF}.orig" ]; then
-    mv -f "${LIVE10_FONTS}/${REGULAR_OTF}" "${LIVE10_FONTS}/${REGULAR_OTF}.orig"
+# merge jp font into AbletonSansBook-Regular.otf
+if [ -f "${JP_PUSH2_FONT}" ]; then
+    ORIG_PUSH2_FONT="${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}"
+    if [ -f "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}.orig" ]; then
+        ORIG_PUSH2_FONT="${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}.orig"
+    fi
+    merge_font "${ORIG_PUSH2_FONT}" "${JP_PUSH2_FONT}" /tmp/_temporary_push2_merged_browser_font.otf
+    JP_PUSH2_FONT=/tmp/_temporary_push2_merged_browser_font.otf
 fi
-mv -f "${SRC_REGULAR_FONT}" "${LIVE10_FONTS}/${REGULAR_OTF}"
 
-if [ ! -f "${LIVE10_FONTS}/${BOLD_OTF}.orig" ]; then
-    mv -f "${LIVE10_FONTS}/${BOLD_OTF}" "${LIVE10_FONTS}/${BOLD_OTF}.orig"
+# swap Live10 regular font
+if [ ! -f "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}.orig" ]; then
+    mv -f "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}" "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}.orig"
 fi
-mv -f "${SRC_BOLD_FONT}" "${LIVE10_FONTS}/${BOLD_OTF}"
+cp -f "${JP_REGULAR_FONT}" "${LIVE10_FONTS_DIR}/${LIVE10_REGULAR_OTF}"
+
+# swap Live10 bold font
+if [ ! -f "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}.orig" ]; then
+    mv -f "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}" "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}.orig"
+fi
+cp -f "${JP_BOLD_FONT}" "${LIVE10_FONTS_DIR}/${LIVE10_BOLD_OTF}"
+
+# swap push2 browser font
+if [ -f "${JP_PUSH2_FONT}" ]; then
+    if [ ! -f "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}.orig" ]; then
+        mv -f "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}" "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}.orig"
+    fi
+    cp -f "${JP_PUSH2_FONT}" "${PUSH2_FONTS_DIR}/${PUSH2_BROWSER_OTF}"
+fi
+
+# cleanup
+if [ -f /tmp/_temporary_live10_jp_regular_font.otf ]; then
+    rm -f /tmp/_temporary_live10_jp_regular_font.otf
+fi
+if [ -f /tmp/_temporary_live10_jp_bold_font.otf ]; then
+    rm -f /tmp/_temporary_live10_jp_bold_font.otf
+fi
+if [ -f /tmp/_temporary_push2_jp_browser_font.otf ]; then
+    rm -f /tmp/_temporary_push2_jp_browser_font.otf
+fi
+if [ -f /tmp/_temporary_push2_merged_browser_font.otf ]; then
+    rm -f /tmp/_temporary_push2_merged_browser_font.otf
+fi
